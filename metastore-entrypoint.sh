0c61a7cf6212a12f82503aa79b20b506274f3d92
“Naeel” <tazet@narod.ru>
2025-09-21 19:18:15 +0300

    5

--- metastore-entrypoint.sh
+++ metastore-entrypoint.sh
@@ -3,9 +3,16 @@ set -e
 
 # Установка classpath из hadoop command
 echo "Setting up Hadoop and Hive classpath..."
-export HADOOP_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath --glob):/opt/hive/lib/*
-export CLASSPATH=$HADOOP_CLASSPATH
+# export HADOOP_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath --glob):/opt/hive/lib/*
+# export CLASSPATH=$HADOOP_CLASSPATH
+
+# export HADOOP_CLASSPATH="/opt/hadoop/*:/opt/hive/lib/*"
+export HADOOP_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath --glob)
 export HADOOP_HOME=/opt/hadoop
+# echo $HADOOP_HOME
+# echo "Classpath - "
+# echo $HADOOP_CLASSPATH
+# echo "- Classpath "
 
 # Создаем директорию если не существует
 if [ ! -d "/home/hive" ]; then
